9538292cfc13f75534eb51ae289db1a774e136df
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2021-03-17 11:00:37 -0600

    Changed to eliminate use of cors-anywhere.

--- model/FetchUtilities.js
+++ model/FetchUtilities.js
@@ -2,16 +2,15 @@ const FetchUtilities = {};
 
 // see https://dev.to/ycmjason/javascript-fetch-retry-upon-failure-3p6g
 FetchUtilities.fetchRetry = (url, options, n) => {
-  const myUrl = `https://cors-anywhere.herokuapp.com/${url}`;
   const myHeaders = new Headers({
-    "x-requested-with": "fetch"
+    "x-requested-with": "fetch",
   });
-  return fetch(myUrl, { mode: "cors", headers: myHeaders })
-    .then(response => {
+  return fetch(url, { mode: "cors", headers: myHeaders })
+    .then((response) => {
       if (response.ok) return response;
       throw Error(`Request rejected with status ${response.status}`);
     })
-    .catch(error => {
+    .catch((error) => {
       if (n === 1) throw error;
       return FetchUtilities.fetchRetry(url, options, n - 1);
     });
@@ -19,7 +18,9 @@ FetchUtilities.fetchRetry = (url, options, n) => {
 
 FetchUtilities.fetchRetryXml = (url, options, n) =>
   FetchUtilities.fetchRetry(url, options, n)
-    .then(response => response.text())
-    .then(responseText => new DOMParser().parseFromString(responseText, "application/xml"));
+    .then((response) => response.text())
+    .then((responseText) =>
+      new DOMParser().parseFromString(responseText, "application/xml")
+    );
 
 export default FetchUtilities;
