98f5536d6cc939185747a5921f6d595c8ac97962
Jeffrey M. Thompson <jmthompson2015@users.noreply.github.com>
2021-04-08 10:07:58 -0600

    Adjusted color on Filters and Columns.

--- app/GameFinder.js
+++ app/GameFinder.js
@@ -57,8 +57,8 @@ GameLoader.load(store).then(() => {
     );
     store.dispatch(ActionCreator.setFilteredReactTable(frt));
 
-    const className = undefined;
-    const headerClass = "f5 gf-bg-light1 ph1 pt1 tl";
+    const className = "gf-bg-light1 ma1";
+    const headerClass = "f5 ph1 pt1 tl";
     ReactDOM.render(
       frt.filterPanel("Filters", className, headerClass),
       document.getElementById("filter")
